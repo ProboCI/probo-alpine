8ae303d45ac3c29ff23da1318a8159af9db6c36a
Michael Bagnall <mrbagnall@icloud.com>
2022-03-06 20:31:09 -0600

    Refinements to versionizer and tuning build scripts for Alpine

--- php8.0/files/mysql-heartbeat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT="Can't connect"
-while [[ $OUTPUT == *"Can't connect"* ]]
-do
-    OUTPUT=$(mysql -hlocalhost -uroot --password=strongpassword < ./my_script.sql 2>&1)
-done
--- php8.0/files/versionizer.php
+++ php8.0/files/versionizer.php
@@ -1,36 +1,34 @@
+#!/bin/php
 <?php
-
 $php_information = explode("\n", `php --version`);
-$apache_information = explode("\n", `apache2 -v`);
-$linux_information = explode("\n", `lsb_release -a`);
-$linux_distribution = str_replace('Description:', '', $linux_information[1]);
+$apache_information = explode("\n", `httpd -v`);
 // ----------------------------------------------------------------------------
 $php_version = $php_information[0];
 $apache_version = $apache_information[0];
-$chrome_version = `google-chrome --version`;
+$chrome_version = `chromium-browser --version`;
 $chromedriver_version = `chromedriver --version`;
 $mysql_version = `mysql --version`;
 $linux_kernel = `uname -r`;
-$linux_distribution = trim($linux_distribution);
 $solr_version = `/opt/solr/bin/solr version`;
 $composer_version = `composer --version`;
 $drush_version = `drush --version`;
 $node_version = `node --version`;
-$wp_cli_version = `wp --version --allow-root`;
+$wp_cli_version = `wp --version --allow-root`; 
 $acli_version = `acli --version`;
 $terminus_version = `terminus --version`;
 $bee_version = `bee version`;
 
 print "\n\n";
+print "\e[1;33m------------------------------------------\n";
 print "Probo.CI - Software Versions Report:\n";
-print "------------------------------------\n";
+print "Alpine Linux Distribution - v3.15\n";
+print "------------------------------------------\e[0m\n";
 print $php_version . "\n";
-print "Web " . $apache_version . "\n";
+print "Apache: " . $apache_version . "\n";
 print $chrome_version;
 print $chromedriver_version;
 print $mysql_version;
 print "Kernel: " . $linux_kernel;
-print $linux_distribution . "\n";
 print "SOLR: " . $solr_version;
 print $composer_version;
 print $drush_version;
