c5af27e6302b33ac39c91b1018c3c5116f5500b0
Michael Bagnall <mrbagnall@icloud.com>
2022-10-02 13:56:31 -0500

    Adjust the build scripts since echo varies platform to platform

--- php8.0/build.sh
+++ php8.0/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------"
-echo -n "Alpine - PHP 8.0 ProboCI Build: "
-docker build . -q -t proboci/alpine:php8.0
+printf "Alpine - PHP 8.0 ProboCI Build: "
+docker build . -q -t proboci/alpine:php8.0 ${2}
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-  echo -n "Pushing to DockerHub: "
+  printf "Pushing to DockerHub: "
   docker push -q proboci/alpine:php8.0
 fi
 
--- php8.1/build.sh
+++ php8.1/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------"
-echo -n "Alpine - PHP 8.1 ProboCI Build: "
-docker build . -q -t proboci/alpine:php8.1
+printf "Alpine - PHP 8.1 ProboCI Build: "
+docker build . -q -t proboci/alpine:php8.1 ${2}
 if [[ ${1} = 'production' ]] || [[ ${1} = 'prod' ]]; then
-  echo -n "Pushing to DockerHub: "
+  printf "Pushing to DockerHub: "
   docker push -q proboci/alpine:php8.1
 fi
 
